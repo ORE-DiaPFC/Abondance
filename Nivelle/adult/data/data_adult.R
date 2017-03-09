500a55acc0bee87e535f6b7142aa9b7dc29fa3ba
matbuoro <matbuoro@github.com>
2017-03-09 10:13:37 +0100

    add data 2016

--- Nivelle/adult/data/data_adult.R
+++ Nivelle/adult/data/data_adult.R
@@ -8,13 +8,14 @@
 
 
 ## WORKING DIRECTORY:
-# work.dir<-paste('~/Documents/RESEARCH/PROJECTS/ORE/Abundance',site,stade,'data/',sep="/")
+# work.dir<-paste('~/Documents/RESEARCH/PROJECTS/ORE/Abundance',site,stade,sep="/")
 # setwd(work.dir)
 
 
 ##-----------------------------DATA ----------------------------------##
 # Nombre d'années suivies:
-years <- read.bugsdata(paste("data/data_list_",year,".txt",sep="")) # Nombre d'années
+years <- read.bugsdata(paste("data/data_list.txt",sep="")) # Nombre d'années
+#years <- read.bugsdata(paste("data/data_list_",year,".txt",sep="")) # Nombre d'années
 Y <- years$Y
 
 ################################################################################
@@ -35,7 +36,7 @@ Cum_EF=as.matrix(EF[,13:16])
 ## OMEGA[t,z]: annual redd count per zone. 1/ LN1 (from Ascain to Uxondoa), 2/ LN2 (from Uxondoa to Olha), 4/ UN, 5/ LUR
 ## /!\ Zone 3 does not exist
 ################################################################################
-REDDS <- read.table(paste("data/data_REDDS_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+REDDS <- read.table(paste("data/data_REDDS.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 REDDS <- as.matrix(REDDS);mode(REDDS)<- "numeric"
 OMEGA<-matrix(NA,nrow = years$Y, ncol = 5)
 OMEGA[,1]<-REDDS[,1]
@@ -46,12 +47,12 @@ OMEGA[,5]<-REDDS[,4]
 
 ### Q[t]: annual mean December flow (m3/S)
 ## Covariate is standardized within BUGS
-Q <- read.table(paste("data/data_Q_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+Q <- read.table(paste("data/data_Q.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 Q <- as.numeric(as.vector(Q[,1]))
 
 ### Eff_Ux is the ratio of the number of trapping nights over the mean number of nights of trapping from 1984 to 2011 (=302 days)
 ## eff_Ol is the ratio of the number of trapping nights over the mean number of nights of trapping from 1994 to 2011 (excluded 1992,1993,2000, mean = 294)
-eff <- read.bugsdata(paste("data/data_eff_",year,".txt",sep=""))
+eff <- read.bugsdata(paste("data/data_eff.txt",sep=""))
 eff_Ux <- eff$eff_Ux 
 eff_Ol <- eff$eff_Ol
 
@@ -67,7 +68,7 @@ eff_Ol <- eff$eff_Ol
 ##   = A_12[t,g] except for the first years (until 1991)																															
 ## Aum_12[t,g]: Annual number of unmarked fish caught by angling between Uxondoa and Olha per breeding category; 1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW"																															
 ################################################################################																															
-dead <- read.table(paste("data/data_dead_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+dead <- read.table(paste("data/data_dead.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 dead <- as.matrix(dead);mode(dead)<- "numeric"
 D_11=as.matrix(dead[,1:4])
 D_12=as.matrix(dead[,5:8])
@@ -82,7 +83,7 @@ Aum_12=as.matrix(dead[,29:32])
 ## Cm_O[t,g]: Annual number of marked fish captured at Olha per breeding category since 2012 (partial trapping);  1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW"							
 ## Cum_O[t,g]: Annual number of unmarked fish captured at Olha per breeding category since 2012 (partial trapping);  1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW"							
 ################################################################################							
-Ol <- read.table(paste("data/data_Ol_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+Ol <- read.table(paste("data/data_Ol.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 Ol <- as.matrix(Ol);mode(Ol)<- "numeric"
 Cm_O=as.matrix(Ol[,1:4])
 Cum_O=as.matrix(Ol[,5:8])
@@ -92,7 +93,7 @@ Cum_O=as.matrix(Ol[,5:8])
 ## Cm_U[t,g]: Annual number of fish captured at Uxondoa and released with mark per breeding category = C_U[t,g] except for 1993 and 1994 ; 1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW
 ## Cum_U[t,g]: Annual number of fish captured at Uxndoa but released unmarked (happened only in 1993 and 1994) per breeding category; 1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW
 ################################################################################
-Ux <- read.table(paste("data/data_Ux_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+Ux <- read.table(paste("data/data_Ux.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 Ux <- as.matrix(Ux);mode(Ux)<- "numeric"
 C_U=as.matrix(Ux[,1:4])
 Cum_U=as.matrix(Ux[,5:8])
@@ -108,7 +109,7 @@ Cm_U=as.matrix(Ux[,9:12])
 ## ech_MSW[,4]: annual number of captured individuals 2R3SW
 ## ech_MSW[,5]: annual number of captured individuals second spawning
 ################################################################################
-age <- read.table(paste("data/data_age_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+age <- read.table(paste("data/data_age.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 age <- as.matrix(age);mode(age)<- "numeric"
 ech_1SW_tot=age[,1]
 ech_1.1SW=age[,2]
@@ -120,7 +121,7 @@ ech_MSW=as.matrix(age[,4:8])
 ## e_2[t,g]: annual breeding escapement in the high catchment zone (UN + LUR) per breeding category; 1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW"			
 ##    = number of fish released upstream Olha = NA until 1989, in 2000 and since 2012			
 ################################################################################			
-EHC <- read.table(paste("data/data_EHC_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+EHC <- read.table(paste("data/data_EHC.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 EHC <- as.matrix(EHC);mode(EHC)<- "numeric"
 e_2=EHC
 
@@ -129,7 +130,7 @@ e_2=EHC
 ## nm_2[t,g]: Annual number of marked fish captured at Olha per breeding category (when trapping is total, globally from 92 to 2011);  1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW
 ## num_2[t,g]: Annual number of unmarked fish captured at Olha per breeding category (when trapping is total, globally from 92 to 2011);  1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW
 ################################################################################
-nm_num <- read.table(paste("data/data_nm-num_2_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+nm_num <- read.table(paste("data/data_nm-num_2.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 nm_num <- as.matrix(nm_num);mode(nm_num)<- "numeric"
 nm_2=as.matrix(nm_num[,1:4])
 num_2=as.matrix(nm_num[,5:8])
@@ -138,7 +139,7 @@ num_2=as.matrix(nm_num[,5:8])
 ## NB[t,g]: annual breeding escapement per breeding category in the high catchment zone added or removed in the HC zone;  1: male 1SW; 2: female 1SW; 3: male MSW; 4: female MSW 
 ## = sum of the number of fish removed from the population and the number of fish released upstream Olah. Happened in 2000 and since 2012.
 ################################################################################
-NB <- read.table(paste("data/data_NB_",year,".txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
+NB <- read.table(paste("data/data_NB.txt",sep=""),header = TRUE, check.names=FALSE,comment.char = "#",colClasses="character")
 NB <- as.matrix(NB);mode(NB)<- "numeric"
 
 
